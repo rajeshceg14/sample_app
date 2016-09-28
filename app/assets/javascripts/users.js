fc735cdc3ba749ac48abd3e4ced6c1c18a371552
Rajesh Kumar S R <rajeshkumar@chronus.com>
2016-09-28 20:38:58 +0530

    Setting Map zoom

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,22 @@
+
+
 function initMap(lat, lng) {
+
+	var mapOptions = {
+	  center: new google.maps.LatLng(0,0),
+	  zoom: 10
+	};
+
+
 	handler = Gmaps.build('Google');
-	handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+	handler.buildMap({ provider: mapOptions, internal: {id: 'map'}}, function(){
 	  markers = handler.addMarkers([
 	    {
 	      "lat": lat,
 	      "lng": lng,
 
 	      "picture": {
-	        "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
+	        "url": "http://icons.iconarchive.com/icons/paomedia/small-n-flat/24/map-marker-icon.png",
 	        "width":  32,
 	        "height": 32
 
@@ -15,8 +24,9 @@ function initMap(lat, lng) {
 	      "infowindow": "hello!"
 	    }
 	  ]);		  
+	  handler.map.centerOn([lat,lng]);	  
 	  handler.bounds.extendWith(markers);
-	  handler.fitMapToBounds();
+	  handler.getMap().setZoom(10);
 	});
 }
 
